98f56da6e548af6d5660f0c42726a5cde17f23e3
Markus Scheidgen <markus.scheidgen@gmail.com>
2013-02-14 17:46:38 +0100

    changed the Referenced class without changing the HelloWorld class and the respective reference between those classes.

--- example.java/src/de/hub/example/java/Referenced.java
+++ example.java/src/de/hub/example/java/Referenced.java
@@ -2,7 +2,12 @@ package de.hub.example.java;
 
 public class Referenced {
 
-	public void run() {
+	public void run() {		
 		System.out.println("Hello world.");
+		log("Hello world printed.");
+	}
+	
+	public void log(String msg) {
+		System.out.println("# " + msg);
 	}
 }
