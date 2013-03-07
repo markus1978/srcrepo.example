69dd1ce62129deac4981f3895368aeb579095175
Markus Scheidgen <markus.scheidgen@gmail.com>
2013-03-07 09:24:25 +0100

    Added some comments.

--- example.java/src/de/hub/example/java/HelloWorld.java
+++ example.java/src/de/hub/example/java/HelloWorld.java
@@ -1,7 +1,21 @@
 package de.hub.example.java;
 
+/**
+ * This is a java doc comment for a top-level class.
+ */
 public class HelloWorld {
+	
+	/*
+	 * This is a comment just somewhere.
+	 */
+	
+	/**
+	 * This is a java doc comment just somewhere.
+	 */
 
+	/**
+	 * This is a java doc commit at a method.
+	 */
 	public static void main(String args) {
 		// a Comment
 		new Referenced().run();
