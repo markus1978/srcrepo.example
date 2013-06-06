be24e206da29dbb03b6e01b6fe23d1f550715112
markus <markus@localhost>
2013-06-06 14:53:17 +0200

    Added a inner class to HelloWorld.

--- example.java/src/de/hub/example/java/HelloWorld.java
+++ example.java/src/de/hub/example/java/HelloWorld.java
@@ -1,9 +1,19 @@
 package de.hub.example.java;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  * This is a java doc comment for a top-level class.
  */
 public class HelloWorld {
+	
+	/**
+	 * This is a innter class doc.
+	 */
+	private abstract class InnerClass<E> extends ArrayList<E> implements Serializable {
+		
+	}
 	
 	/*
 	 * This is a comment just somewhere.
